916317ab1d3c06248774a33dd69c08dc2744b371
elevin1 <ellie.liu@education.nsw.gov.au>
2021-10-26 20:38:43 +1100

    05

---
+++ 05forLoop.py
@@ -0,0 +1,10 @@
+print( " ------------------------------------------------")
+print("|                                                |")
+print("|    05forLoop                                   |")
+print("|    Name : Ellie Liu                            |")
+print("|    Version : 01                                |")
+print("|                                                |")
+print( " ------------------------------------------------")
+
+for x in range(1, 500):
+  print(x)
