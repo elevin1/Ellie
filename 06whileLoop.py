e395c5af3360213b4e50fbbe59dccc939f6d2f29
elevin1 <ellie.liu@education.nsw.gov.au>
2021-10-27 12:21:43 +1100

    asdklfhsadkf

---
+++ 06whileLoop.py
@@ -0,0 +1,21 @@
+print( " ------------------------------------------------")
+print("|                                                |")
+print("|    06whileLoop                                 |")
+print("|    Name : Ellie Liu                            |")
+print("|    Version : 01                                |")
+print("|                                                |")
+print( " ------------------------------------------------")
+
+i = 0
+
+while i == 0:
+    subject = input("What is the name of this subject ")
+    if subject ==  "IST":
+        print("")
+        print("")
+        print("Congratulations!!")
+        print("")
+        print("")
+        break
+    else: 
+        print("Not Correct - try again")
